5b0471a2e4ced81fec3ce0a95e500f7bdfb63567
Victor Khristoforov <cogu97@gmail.com>
2023-03-13 04:06:40 +0700

    added wheat grow

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -2,4 +2,29 @@ using UnityEngine;
 
 public class Field : MonoBehaviour, IField
 {
+    [SerializeField] private GameObject _cropPrefab;
+    [SerializeField] private Vector2 _fieldSize;
+    [SerializeField] private Vector2Int _gridSize;
+
+
+    private void Start()
+    {
+        GenerateGrid();
+    }
+
+
+    private void GenerateGrid()
+    {
+        float stepX = _fieldSize.x / (_gridSize.x - 1);
+        float stepY = _fieldSize.y / (_gridSize.y - 1);
+        Vector3 offset = new Vector3(-_fieldSize.x * 0.5f, 0, -_fieldSize.y * 0.5f);
+        for (int x = 0; x < _gridSize.x; x++)
+        {
+            for (int y = 0; y < _gridSize.y; y++)
+            {
+                var instance = Instantiate(_cropPrefab, transform);
+                instance.transform.localPosition = new Vector3(stepX * x, 0, stepY * y) + offset;
+            }
+        }
+    }
 }
--- Assets/Scripts/Grass.cs
+++ Assets/Scripts/Grass.cs
@@ -1,21 +1,52 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Grass : MonoBehaviour, ICollectible
 {
-    [SerializeField] private GameObject _model;
     [SerializeField] private GameObject _pickup;
     [SerializeField] private bool _ready;
+    [SerializeField] private float _growTime;
+
+
+    private Material _material;
 
 
     public bool Ready => _ready;
 
 
+    private void Start()
+    {
+        if (!(
+            TryGetComponent(out MeshRenderer meshRenderer)
+            ))
+        {
+            Debug.LogError("Error occured while initing!");
+            Destroy(this);
+        }
+
+        _material = meshRenderer.material;
+    }
+
+
     public GameObject Collect()
     {
-        _model.SetActive(false);
+        _material.SetFloat("_Progress", 0);
         _ready = false;
-        return _pickup;
+        StartCoroutine(Grow());
+        return Instantiate(_pickup);
+    }
+
+    private IEnumerator Grow()
+    {
+        float time = 0;
+        while (time < _growTime)
+        {
+            _material.SetFloat("_Progress", time / _growTime);
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        _material.SetFloat("_Progress", 1);
+        _ready = true;
     }
 }
